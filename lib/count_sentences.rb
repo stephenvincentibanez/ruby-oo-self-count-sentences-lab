ff9fc6ac24ef7150621a1ce0cecd08c58d3c7827
Stephen Ibanez <sibanez93@Stephens-MacBook-Pro-3.local>
2020-10-10 14:43:40 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,14 @@ class String
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).reject {|string| string.empty?}.length
   end
 end
